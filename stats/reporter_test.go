b26d6c4672d1fd8f063ab3a4a5fd0589b742cec8
Prashant Varanasi <prashant@uber.com>
2016-01-20 23:27:00 -0800

    Add a benchmark for addTagsToName

--- stats/reporter_test.go
+++ stats/reporter_test.go
@@ -2,6 +2,18 @@ package stats
 
 import "testing"
 
+func BenchmarkAddTagsToName(b *testing.B) {
+	tags := map[string]string{
+		"host":     "myhost",
+		"endpoint": "hello",
+		"os":       "OS X",
+		"browser":  "Chrome",
+	}
+	for i := 0; i < b.N; i++ {
+		addTagsToName("recv.calls", tags)
+	}
+}
+
 func TestAddTagsToName(t *testing.T) {
 	tests := []struct {
 		name     string
